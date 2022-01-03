157372f0c6fa33ac4925548ea908dea3460f5b57
Will Seymour <willseymour804@gmail.com>
2022-01-03 17:04:30 -0600

    All graphs functioning basic first draph

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -40,7 +40,7 @@ conn.close()
 # Application layout
 app.layout = html.Div(children=[
 	# Title
-	html.H1('Sales Report'),
+	html.H1('Sales Reporting Dashboard'),
 	# Row 1
 	html.Div([
 		# Graph 1
@@ -50,7 +50,7 @@ app.layout = html.Div(children=[
 						id='segment-input',
 						options=[{'label': i, 'value': i} for i in STATES],
 						placeholder="Select a state",
-						style= {'width': 500, 'padding': 3}
+						style= {'width': 300, 'padding': 3}
 						),
 				dcc.Graph(id='segment-plot')
 				], style= {'padding': 3}, className='six columns'),
@@ -61,7 +61,7 @@ app.layout = html.Div(children=[
 						id='ship-input',
 						options=[{'label': i, 'value': i} for i in STATES],
 						placeholder="Select a state",
-						style= {'width': 500, 'padding': 3}
+						style= {'width': 300, 'padding': 3}
 						),
 				dcc.Graph(id='ship-plot')
 				], style= {'padding': 3}, className='six columns')
@@ -72,23 +72,23 @@ app.layout = html.Div(children=[
 		html.Div([
 				"Input: ",
 				dcc.Dropdown(
-						id='input3',
+						id='del-input',
 						options=[{'label': i, 'value': i} for i in STATES],
 						placeholder="Select a state",
-						style= {'width': 500, 'padding': 3}
+						style= {'width': 300, 'padding': 3}
 						),
-				dcc.Graph(id='plot3')
+				dcc.Graph(id='del-plot')
 				], style= {'padding': 3}, className='six columns'),
 		# Graph 4
 		html.Div([
 							"Input: ",
 				dcc.Dropdown(
-						id='input4',
+						id='sale-input',
 						options=[{'label': i, 'value': i} for i in STATES],
 						placeholder="Select a state",
-						style= {'width': 500, 'padding': 3}
+						style= {'width': 300, 'padding': 3}
 						),
-				dcc.Graph(id='plot4')
+				dcc.Graph(id='sale-plot')
 			], style= {'padding': 3}, className='six columns')
 	], className='twelve columns')
 
@@ -96,7 +96,7 @@ app.layout = html.Div(children=[
 	
 
 
-
+# Callback for the top left bar chart of profit by segment and state
 @app.callback(Output(component_id='segment-plot', component_property='figure'),
 				Input(component_id='segment-input', component_property='value'))
 def get_segment_graph(segment_input):
@@ -105,28 +105,53 @@ def get_segment_graph(segment_input):
 	segment_data = df.groupby(['State', 'Segment'])['Profit'].sum().reset_index()
 	segment_data = segment_data[segment_data['State'] == segment_input]
 
-	# Build the graph
+	# Build the bar chart
 	bar_fig = px.bar(segment_data, x='Segment', y='Profit')
 
 	return bar_fig
 
 
-
+# Callback for the top right pie chart of ship mode requested by state
 @app.callback(Output(component_id='ship-plot', component_property='figure'),
 				Input(component_id='ship-input', component_property='value'))
 def get_shipment_graph(ship_input):
-	df = get_data()
-
-	ship_data = df.groupby('State')['Ship Mode'].value_counts()
+	ship_data = get_data()
+	# Get the required dataset from the sales data 
+	ship_data = ship_data.groupby('State')['Ship Mode'].value_counts()
 	ship_data = ship_data[ship_input]
 	ship_data = ship_data.to_frame()
 	ship_data = ship_data.rename(columns={'Ship Mode': 'Mode', 'Ship Mode': 'Value'})
 	ship_data.reset_index(inplace=True)
+	# Build the pie chart 
 	ship_fig = px.pie(ship_data, values='Value', names='Ship Mode')
 
 	return ship_fig
 
 
+# Callback for the bottom left scatter plot of delivery times by state
+@app.callback(Output(component_id='del-plot', component_property='figure'),
+				Input(component_id='del-input', component_property='value'))
+def get_delivery_graph(del_input):
+	del_data = get_data()
+	# Get the required dataset
+	del_data = del_data[del_data['State'] == del_input]
+	# build the figure
+	del_figure = px.scatter(del_data, x='Order Date', y='Delivery Time')
+
+	return del_figure
+
+
+@app.callback(Output(component_id='sale-plot', component_property='figure'),
+				Input(component_id='sale-input', component_property='value'))
+def get_sales_graph(sale_input):
+	sale_data = get_data()
+
+	sale_data = sale_data.groupby(['Category', 'State'])['Sales'].sum().reset_index()
+	sale_data = sale_data[sale_data['State']==sale_input]
+
+	sale_fig = px.pie(sale_data, values='Sales', names='Category')
+
+	return sale_fig
 
 
 # Run the app
