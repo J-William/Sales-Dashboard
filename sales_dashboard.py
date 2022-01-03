d5ac216045a40a18804de213cee1c35bfa100e01
Will Seymour <willseymour804@gmail.com>
2022-01-03 14:44:45 -0600

    Multiple graph framework setup; two graphs built

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -45,7 +45,7 @@ app.layout = html.Div(children=[
 	html.Div([
 		# Graph 1
 		html.Div([
-				"Input: ",
+				html.H5('Profit by Segment and State'),
 				dcc.Dropdown(
 						id='segment-input',
 						options=[{'label': i, 'value': i} for i in STATES],
@@ -56,14 +56,14 @@ app.layout = html.Div(children=[
 				], style= {'padding': 3}, className='six columns'),
 		# Graph 2
 		html.Div([
-				"Input: ",
+				html.H5('Ship Mode Requested by State'),
 				dcc.Dropdown(
-						id='input2',
+						id='ship-input',
 						options=[{'label': i, 'value': i} for i in STATES],
 						placeholder="Select a state",
 						style= {'width': 500, 'padding': 3}
 						),
-				dcc.Graph(id='plot2')
+				dcc.Graph(id='ship-plot')
 				], style= {'padding': 3}, className='six columns')
 	], className='twelve columns'),
 	# Row 2
@@ -99,18 +99,32 @@ app.layout = html.Div(children=[
 
 @app.callback(Output(component_id='segment-plot', component_property='figure'),
 				Input(component_id='segment-input', component_property='value'))
-def get_graph(state):
+def get_segment_graph(segment_input):
 	# Get the data for the selected state
 	df = get_data()
-	data = df.groupby(['State', 'Segment'])['Profit'].sum().reset_index()
-	data = data[data['State'] == state]
+	segment_data = df.groupby(['State', 'Segment'])['Profit'].sum().reset_index()
+	segment_data = segment_data[segment_data['State'] == segment_input]
 
 	# Build the graph
-	bar_fig = px.bar(data, x='Segment', y='Profit', title="{} Profit by Segment".format(state))
+	bar_fig = px.bar(segment_data, x='Segment', y='Profit')
 
 	return bar_fig
 
 
+
+@app.callback(Output(component_id='ship-plot', component_property='figure'),
+				Input(component_id='ship-input', component_property='value'))
+def get_shipment_graph(ship_input):
+	df = get_data()
+
+	ship_data = df.groupby('State')['Ship Mode'].value_counts()
+	ship_data = ship_data[ship_input]
+	ship_data = ship_data.to_frame()
+	ship_data = ship_data.rename(columns={'Ship Mode': 'Mode', 'Ship Mode': 'Value'})
+	ship_data.reset_index(inplace=True)
+	ship_fig = px.pie(ship_data, values='Value', names='Ship Mode')
+
+	return ship_fig
 
 
 
