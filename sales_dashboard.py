ad91dc0aaa2e012ff96601aa5c1ce25d013d2aff
Will Seymour <willseymour804@gmail.com>
2022-01-03 12:37:30 -0600

    Basic layout inplace data connection working, input being read and graph being returned.

--- sales_dashboard.py
+++ sales_dashboard.py
@@ -18,16 +18,23 @@ app = dash.Dash(__name__)
 # Clear layout and don't display exceptions until callback
 app.config.suppress_callback_exceptions = True
 
+
+
+
 # Retrieve the data from the database
 def get_data():
 	conn = sqlite3.connect('sales.db')
 	data = pd.read_sql_query("SELECT * FROM sales", conn)
 	conn.close()
 	return data
-	
 
 
-
+# Get the list of states from db
+conn = sqlite3.connect('sales.db')
+STATES = pd.read_sql_query("SELECT * FROM states", conn)
+STATES = STATES['State']
+STATES = sorted(list(STATES))
+conn.close()
 
 
 
@@ -36,10 +43,16 @@ app.layout = html.Div(children=[
 
 	html.H1('Sales Report'),
 	html.Div([
-			"Input: ",
-			dcc.Input(id='input', value='Alabama', type='text')
-			]),
-	dcc.Graph(id='plot1')
+		html.Div([
+				"Input: ",
+				dcc.Dropdown(
+						id='input',
+						options=[{'label': i, 'value': i} for i in STATES],
+						placeholder="Select a state"
+						)
+				]),
+		dcc.Graph(id='plot1')
+		])
 ])
 	
 
@@ -51,7 +64,7 @@ def get_graph(state):
 	# Get the data for the selected state
 	df = get_data()
 	data = df.groupby(['State', 'Segment'])['Profit'].sum().reset_index()
-	data = data[data['State'] == 'Alabama']
+	data = data[data['State'] == state]
 
 	# Build the graph
 	bar_fig = px.bar(data, x='Segment', y='Profit', title="{} Profit by Segment".format(state))
